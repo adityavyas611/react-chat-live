a0298aa3f40d74cc6a972b8686a9dc4f73b27203
Aditya Vyas <vyasaditya41@yahoo.in>
2020-02-18 19:19:09 +0530

    Add: heroku server

--- client/src/components/Chat/Chat.component.jsx
+++ client/src/components/Chat/Chat.component.jsx
@@ -14,7 +14,7 @@ const Chat = ({ location }) => {
     const [room, setRoom] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://react-chat-live.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
